96ac7524427ae2cb0547256aa2f6271e0a58cdad
Jairo Holanda <jairoholanda007@gmail.com>
2025-07-23 05:52:07 -0300

    As telas de início e sobre foram atualizadas.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <h1>Sobre</h1>
-    <p>Esta é a página "Sobre" do EduLivre.</p>
+  <div class="about-container">
+    <h1 class="about-title">Sobre</h1>
+    <p class="about-description">Este projeto foi desenvolvido para estudos sobre Bancos de Dados, especialmente PostgreSQL. Elaborado e desenvolvido por André Felipe e Jairo Holanda.</p>
   </div>
 </template>
+
+<style scoped>
+* {
+  font-family: 'TikTok Sans', sans-serif;
+}
+
+.about-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+.about-title {
+  color: #111827;
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+}
+
+.about-description {
+  color: #4b5563;
+  font-size: 1.25rem;
+  line-height: 1.75;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="layout">
-    Bem-vindo ao EduLivre!
+  <div class="home-container">
+    <h1 class="home-title">Bem-vindo ao EduLivre!</h1>
   </div>
 </template>
 
 <style scoped>
-.layout {
-  font-family: "TikTok Sans", sans-serif;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+* {
+  font-family: 'TikTok Sans', sans-serif;
+}
+
+.home-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+.home-title {
+  color: #111827;
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
 }
 </style>
