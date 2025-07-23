20cb92dda64762bfd4ef9afe17a788f13b8b1c87
Jairo Holanda <jairoholanda007@gmail.com>
2025-07-22 23:04:55 -0300

    Algumas modificações de setup.

---
+++ src/views/WorkshopsView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import WorkshopCard from '../components/WorkshopCard.vue'
+const workshops = [
+  { id: 1, title: 'Oficina de Vue.js' },
+  { id: 2, title: 'Oficina de Design' }
+];
+</script>
+
+<template>
+  <div>
+    <h1>Nossas Oficinas</h1>
+    <div class="workshop-list">
+      <WorkshopCard v-for="ws in workshops" :key="ws.id" :workshop="ws" />
+    </div>
+  </div>
+</template>
